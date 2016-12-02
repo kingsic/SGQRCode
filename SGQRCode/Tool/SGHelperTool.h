572f35c0c99b750304e4064aba59a74bc9d37211
kingsic <kingsic@126.com>
2016-12-02 11:09:35 +0800

    新增 SGHelperTool，对生成二维码代码进行封装

---
+++ SGQRCode/Tool/SGHelperTool.h
@@ -0,0 +1,27 @@
+//
+//  SGHelperTool.h
+//  SGQRCodeExample
+//
+//  Created by apple on 16/12/2.
+//  Copyright © 2016年 JP_lee. All rights reserved.
+//
+//  - - - - - - - - - - - - - - 交流QQ：1357127436 - - - - - - - - - - - - - - - //
+//
+//  - - 如在使用中, 遇到什么问题或者有更好建议者, 请于 kingsic@126.com 邮箱联系 - - - - //
+//  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//  - - GitHub下载地址 https://github.com/kingsic/SGQRCode.git - - - - - - - - - //
+//
+//  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+
+#import <UIKit/UIKit.h>
+#import <CoreImage/CoreImage.h>
+
+@interface SGHelperTool : NSObject
+/** 生成一张普通的二维码 */
++ (UIImage *)SG_generateWithDefaultQRCodeData:(NSString *)data imageViewWidth:(CGFloat)imageViewWidth;
+/** 生成一张带有logo的二维码 */
++ (UIImage *)SG_generateWithLogoQRCodeData:(NSString *)data logoImageName:(NSString *)logoImageName logoWidth:(CGFloat)logoWidth;
+/** 生成一张彩色的二维码 */
++ (CIImage *)SG_generateWithColorQRCodeData:(NSString *)data backgroundColor:(CIColor *)backgroundColor mainColor:(CIColor *)mainColor;
+
+@end
