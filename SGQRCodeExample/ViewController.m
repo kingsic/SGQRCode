2b36d9845e968d2938a9119bb1c4ff47400414c3
kingsic <kingsic@126.com>
2018-02-12 13:32:55 +0800

    代码合并

--- SGQRCodeExample/ViewController.m
+++ SGQRCodeExample/ViewController.m
@@ -50,107 +50,53 @@
     }
     
     if (indexPath.row == 1) {
-        [self WBQRCodeScan];
+        WBQRCodeScanningVC *WBVC = [[WBQRCodeScanningVC alloc] init];
+        [self QRCodeScanVC:WBVC];
     }
     
     if (indexPath.row == 2) {
-        [self WCQRCodeScan];
+        WCQRCodeScanningVC *WCVC = [[WCQRCodeScanningVC alloc] init];
+        [self QRCodeScanVC:WCVC];
     }
 }
 
-- (void)WBQRCodeScan {
+- (void)QRCodeScanVC:(UIViewController *)scanVC {
     AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
     if (device) {
         AVAuthorizationStatus status = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
         switch (status) {
-            case AVAuthorizationStatusNotDetermined: {
-                [AVCaptureDevice requestAccessForMediaType:AVMediaTypeVideo completionHandler:^(BOOL granted) {
-                    if (granted) {
-                        dispatch_sync(dispatch_get_main_queue(), ^{
-                            WBQRCodeScanningVC *vc = [[WBQRCodeScanningVC alloc] init];
-                            [self.navigationController pushViewController:vc animated:YES];
-                        });
-                        NSLog(@"用户第一次同意了访问相机权限 - - %@", [NSThread currentThread]);
-                    } else {
-                        NSLog(@"用户第一次拒绝了访问相机权限 - - %@", [NSThread currentThread]);
-                    }
-                }];
-                break;
-            }
-            case AVAuthorizationStatusAuthorized: {
-                WBQRCodeScanningVC *vc = [[WBQRCodeScanningVC alloc] init];
-                [self.navigationController pushViewController:vc animated:YES];
-                break;
-            }
-            case AVAuthorizationStatusDenied: {
-                UIAlertController *alertC = [UIAlertController alertControllerWithTitle:@"温馨提示" message:@"请去-> [设置 - 隐私 - 相机 - SGQRCodeExample] 打开访问开关" preferredStyle:(UIAlertControllerStyleAlert)];
-                UIAlertAction *alertA = [UIAlertAction actionWithTitle:@"确定" style:(UIAlertActionStyleDefault) handler:^(UIAlertAction * _Nonnull action) {
+                case AVAuthorizationStatusNotDetermined: {
+                    [AVCaptureDevice requestAccessForMediaType:AVMediaTypeVideo completionHandler:^(BOOL granted) {
+                        if (granted) {
+                            dispatch_sync(dispatch_get_main_queue(), ^{
+                                [self.navigationController pushViewController:scanVC animated:YES];
+                            });
+                            NSLog(@"用户第一次同意了访问相机权限 - - %@", [NSThread currentThread]);
+                        } else {
+                            NSLog(@"用户第一次拒绝了访问相机权限 - - %@", [NSThread currentThread]);
+                        }
+                    }];
+                    break;
+                }
+                case AVAuthorizationStatusAuthorized: {
+                    [self.navigationController pushViewController:scanVC animated:YES];
+                    break;
+                }
+                case AVAuthorizationStatusDenied: {
+                    UIAlertController *alertC = [UIAlertController alertControllerWithTitle:@"温馨提示" message:@"请去-> [设置 - 隐私 - 相机 - SGQRCodeExample] 打开访问开关" preferredStyle:(UIAlertControllerStyleAlert)];
+                    UIAlertAction *alertA = [UIAlertAction actionWithTitle:@"确定" style:(UIAlertActionStyleDefault) handler:^(UIAlertAction * _Nonnull action) {
+                        
+                    }];
                     
-                }];
+                    [alertC addAction:alertA];
+                    [self presentViewController:alertC animated:YES completion:nil];
+                    break;
+                }
+                case AVAuthorizationStatusRestricted: {
+                    NSLog(@"因为系统原因, 无法访问相册");
+                    break;
+                }
                 
-                [alertC addAction:alertA];
-                [self presentViewController:alertC animated:YES completion:nil];
-                break;
-            }
-            case AVAuthorizationStatusRestricted: {
-                NSLog(@"因为系统原因, 无法访问相册");
-                break;
-            }
-                
-            default:
-                break;
-        }
-        return;
-    }
-    
-    UIAlertController *alertC = [UIAlertController alertControllerWithTitle:@"温馨提示" message:@"未检测到您的摄像头" preferredStyle:(UIAlertControllerStyleAlert)];
-    UIAlertAction *alertA = [UIAlertAction actionWithTitle:@"确定" style:(UIAlertActionStyleDefault) handler:^(UIAlertAction * _Nonnull action) {
-        
-    }];
-    
-    [alertC addAction:alertA];
-    [self presentViewController:alertC animated:YES completion:nil];
-}
-
-- (void)WCQRCodeScan {
-    AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
-    if (device) {
-        AVAuthorizationStatus status = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
-        switch (status) {
-            case AVAuthorizationStatusNotDetermined: {
-                [AVCaptureDevice requestAccessForMediaType:AVMediaTypeVideo completionHandler:^(BOOL granted) {
-                    if (granted) {
-                        dispatch_sync(dispatch_get_main_queue(), ^{
-                            WCQRCodeScanningVC *vc = [[WCQRCodeScanningVC alloc] init];
-                            [self.navigationController pushViewController:vc animated:YES];
-                        });
-                        NSLog(@"用户第一次同意了访问相机权限 - - %@", [NSThread currentThread]);
-                    } else {
-                        NSLog(@"用户第一次拒绝了访问相机权限 - - %@", [NSThread currentThread]);
-                    }
-                }];
-                break;
-            }
-            case AVAuthorizationStatusAuthorized: {
-                WCQRCodeScanningVC *vc = [[WCQRCodeScanningVC alloc] init];
-                [self.navigationController pushViewController:vc animated:YES];
-                break;
-            }
-            case AVAuthorizationStatusDenied: {
-                UIAlertController *alertC = [UIAlertController alertControllerWithTitle:@"温馨提示" message:@"请去-> [设置 - 隐私 - 相机 - SGQRCodeExample] 打开访问开关" preferredStyle:(UIAlertControllerStyleAlert)];
-                UIAlertAction *alertA = [UIAlertAction actionWithTitle:@"确定" style:(UIAlertActionStyleDefault) handler:^(UIAlertAction * _Nonnull action) {
-                    
-                }];
-                
-                [alertC addAction:alertA];
-                [self presentViewController:alertC animated:YES completion:nil];
-                break;
-            }
-            case AVAuthorizationStatusRestricted: {
-                NSLog(@"因为系统原因, 无法访问相册");
-                break;
-            }
-
             default:
                 break;
         }
@@ -168,5 +114,4 @@
 
 
 @end
-
 
